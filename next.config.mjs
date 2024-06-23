69320146fe8a9b1b7083589a978d09c7011c1a85
devshahoriar <shuvo1556@gmail.com>
2024-06-24 01:58:42 +0600

    modified:   next.config.mjs 	modified:   package.json 	modified:   pnpm-lock.yaml 	modified:   src/app/not-found.tsx 	modified:   src/components/ui/aimate/hover-border-gradient.tsx

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  experimental: {
-    reactCompiler: true,
-    turbo: {
-      resolveExtensions: [
-        '.mdx',
-        '.tsx',
-        '.ts',
-        '.jsx',
-        '.js',
-        '.mjs',
-        '.json',
-      ],
-    },
-  },
 }
 
 export default nextConfig
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link'
+
 const NotFound = () => {
   return (
     <div className="h-screen w-screen flex justify-center items-center flex-col">
       <h1 className="text-6xl font-bold">Not found 404</h1>
       <p className="mt-10">This may not build yet😐</p>
+      <Link className="mt-5 hover:underline" href={'/'}>
+        Go to home🏠
+      </Link>
     </div>
   )
 }
